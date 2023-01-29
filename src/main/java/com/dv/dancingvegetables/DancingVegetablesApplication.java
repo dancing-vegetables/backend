d757ce4b13f52a8314e95a69b23399b06ba0c2ea
BAEKDOOSAN <doosan0425@gmail.com>
2023-01-29 17:00:34 +0900

    git ignore setting

---
+++ src/main/java/com/dv/dancingvegetables/DancingVegetablesApplication.java
@@ -0,0 +1,13 @@
+package com.dv.dancingvegetables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DancingVegetablesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DancingVegetablesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dv/dancingvegetables/DancingVegetablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dv.dancingvegetables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DancingVegetablesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
